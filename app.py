7491409aa21efece11063559dcd964ea8fca8ced
Kevin Cochran <40193276+kevincloud@users.noreply.github.com>
2023-02-24 00:50:27 -0500

    initiali commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+from flask import make_response
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def show_page():
+    retval = make_response(render_template("index.html"))
+    return retval
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
